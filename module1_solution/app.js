b0684eb6d3ab3aaa8701784a42b1dc4cb42dda9d
VeeramaniKoppana <veeramanikoppana@gmail.com>
2023-02-06 16:30:33 +0530

    commit

--- module1_solution/app.js
+++ module1_solution/app.js
@@ -6,15 +6,18 @@
         $scope.checkItems = function(){
             if(!$scope.items) {
                 $scope.message = "Please enter data first";
+                $scope.color_change ="red";
             }
             else{
                 var list_of_items = ($scope.items).split(',');
+                list_of_items = list_of_items.filter(function(e){return e}); 
                 if(list_of_items.length<=3) {
                     $scope.message = "Enjoy!";
                 }
                 else {
                     $scope.message = "Too much!";
                 }
+                $scope.color_change ="green";
             }
         };
     };
